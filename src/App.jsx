a30eb5e33ab7d04cd533a1ae1e4e140b8e4a9746
Thiago Lima <lietlaw364@gmail.com>
2022-07-23 14:58:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UseRefExample1 from './components/UseRefExample1'
+
+function App() {
+
+  return (
+    <div className="App">
+      <UseRefExample1 />
+    </div>
+  )
+}
+
+export default App
